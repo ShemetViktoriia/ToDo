1278ef83c50dfc8865bfc1dc5dd41ecde0d890ba
ShemetViktoriia <shemetvic@ukr.net>
2019-12-26 13:14:10 +0200

    Repository was added in project

---
+++ ToDo_Repository/Interface/IToDoItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ToDo_DAL;
+
+namespace ToDo_Repository
+{
+    public interface IToDoItemRepository
+    {
+        ToDoItem Add(ToDoItem entity);
+        ToDoItem FindById(int id);
+        IEnumerable<ToDoItem> FindAll();
+        void Edit(ToDoItem entity);
+    }
+}
---
+++ ToDo_Repository/Repository/ToDoItemRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ToDo_DAL;
+
+namespace ToDo_Repository
+{
+    public class ToDoItemRepository: IToDoItemRepository
+    {
+        protected readonly DbContext _context;
+        protected readonly IDbSet<ToDoItem> _dbset;
+
+        public ToDoItemRepository(DbContext context)
+        {
+            _context = context;
+            _dbset = context.Set<ToDoItem>();
+        }
+
+        public ToDoItem Add(ToDoItem entity)
+        {
+            return _dbset.Add(entity);
+        }
+
+        public void Edit(ToDoItem entity)
+        {
+            _dbset.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IEnumerable<ToDoItem> FindAll()
+        {
+            return _dbset.AsEnumerable();
+        }
+
+        public ToDoItem FindById(int id)
+        {
+            return _dbset.Find(id);
+        }
+    }
+}
